00b9dc8953d6bc15ad910df742181fc7462dde0d
Andrei Baliuk <wolfdogmman@gmail.com>
2023-12-06 15:20:20 +0300

    second commit

--- app/src/main/java/com/example/homework1/MainActivity.kt
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -7,10 +7,10 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    val fig1 = Geometry.Square()
-    val fig2 = Geometry.Rectangle()
-    val fig3 = Geometry.Quadrangle()
-    val fig4 = Geometry.Circumference()
+    val fig_1 = Geometry.Square()
+    val fig_2 = Geometry.Rectangle()
+    val fig_3 = Geometry.Quadrangle()
+    val fig_4 = Geometry.Circumference()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,16 +21,16 @@ class MainActivity : AppCompatActivity() {
         var per1 = findViewById<TextView>(R.id.Plosh)
         var per2 = findViewById<EditText>(R.id.figure)
         if(per2.hint == "Circle"){
-            per1.text = fig1.toSquare1()
+            per1.text = fig_1.toSquare1()
         }
        if(per2.hint == "Rectangle") {
-            per1.text = fig2.toSquare2()
+            per1.text = fig_2.toSquare2()
         }
       if(per2.hint == "Quadrilateral") {
-            per1.text = fig3.toSquare3()
+            per1.text = fig_3.toSquare3()
         }
      if(per2.hint == "Circle") {
-            per1.text = fig4.toSquare4()
+            per1.text = fig_4.toSquare4()
         }
     }
 }
